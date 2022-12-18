50890ec26ad33ab9c21af7282ebb9ffac5cbc528
SuryaSanthosh <sooryasanthosh7@gmail.com>
2022-12-18 21:09:48 +0530

    second commit

---
+++ foot.py
@@ -0,0 +1,3 @@
+foot=int(input("Enter the distance in feet:"))
+inch=foot*12
+print("The corresponding value for inch is:",inch)
---
+++ km.py
@@ -0,0 +1,3 @@
+km=int(input("Enter the km to convert:"))
+miles=km*0.612
+print("The corresponding mile values is:",miles)
